a7339b69ec57e66b49792658d51aa771b5d1f93e
Arnaud jérémy <arnaud.jmy@gmail.com>
2019-09-17 11:40:40 +0200

    portfolio

---
+++ anim.js
@@ -0,0 +1,91 @@
+
+// stop stalker
+
+
+//info perso
+
+
+
+
+
+    $(document).ready(function(){
+        $("#Title_image").click(function(){
+
+          
+            $("#img").fadeToggle('slow');
+            $("#website").fadeToggle('slow');
+            $(".title_clk").fadeToggle('slow');
+            $(".title_rpg").fadeToggle('slow');
+
+
+
+        });
+        $("#info").on('click',function(){
+
+            $(".pitch").fadeToggle('3000');
+            
+            
+        });
+
+    });
+
+
+
+
+
+
+
+
+    // $(function(){
+        
+    //     $("#Title_image").on('mouseover',function(){
+            
+    //         $("#img").show();
+    //         $("#website").show();
+    //         $(".title_clk").show();
+    //         $(".title_rpg").show();
+
+    //     });
+        
+       
+
+            // $("#img").load(function() {
+            //     if($(this).opacity < 1){
+            //         $(this).opacity += 0.2;
+
+            //     }
+                
+            // })
+           
+
+
+
+
+
+
+        
+        
+        
+        
+        
+    // });
+        // $("#Title_image").click(function(){
+
+        //     $("#img").addclass('in').removeclass('hide');
+        //     $("#website").addclass('in').removeclass('hide');
+        //     $(".title_clk").addclass('in').removeclass('hide');
+        //     $(".title_rpg").addclass('in').removeclass('hide');
+
+        // });
+
+
+        //  $("#Title_image").on('click', function(){
+
+        //     $("#img").hide();
+        //     $("#website").hide();
+        //     $(".title_clk").hide();
+        //     $(".title_rpg").hide();
+            
+        //  });
+
+        // });
